741c6c8b1b51cbd5f56f80cc1f7a7a4c9138fc73
erhan11 <77719565+erhan11@users.noreply.github.com>
2022-07-13 22:35:30 +0300

    react version 18 updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./js/App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
